aa340276f7d080719d763216d9ece96bc39a8ad2
p1uma1 <nimsarapiumalsac@gmail.com>
2026-09-19 02:51:32 +0530

    text with particles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let particles = [];
 const mouse = {
     x: null,
     y: null,
-    radius: 150
+    radius: 20
 }
 
 window.addEventListener("mousemove", (event) => {
@@ -20,13 +20,15 @@ window.addEventListener("mousemove", (event) => {
 
 context.font = '30px Verdana';
 context.fillStyle = "white";
-context.fillText('A', 20, 40)
+context.fillText('HELLO', 40, 20)
+
+const textCordinates = context.getImageData(0,0,100,100)
 
 class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = 3;
+        this.size = 1;
         this.density = (Math.random() * 30) + 1;
         this.targetX = this.x;
         this.targetY = this.y;
@@ -39,12 +41,45 @@ class Particle {
         context.closePath();
         context.fill();
     }
+
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let forceDirectionX = dx / distance;
+        let forceDirectionY = dy / distance;
+        let force = (distance - mouse.radius) / mouse.radius;
+        if (distance < 50) {
+            this.x += forceDirectionX * force * this.density;
+            this.y += forceDirectionY * force * this.density;
+        }
+        else {
+            if(this.x !=this.targetX){
+                this.x += (this.targetX-this.x)/10
+            }
+            if(this.y !=this.targetY){
+                this.y += (this.targetY-this.y)/10
+            }
+        }
+    }
 }
 
 function init() {
     particles = [];
-    for(let i = 0 ; i<10;i++){
-        particles.push(new Particle(Math.random()*canvas.width, Math.random()*canvas.height));
+
+    for( let y=0, y2 = textCordinates.height; y<y2; y++){
+        for(let x =0, x2 = textCordinates.width; x<x2;x++){
+
+            if(textCordinates.data[4*(y*textCordinates.width+x)]>128){
+                /*
+                the data hold 
+                4  8bit value for each pixel ( rgb + opacity)
+                here im checking opacity
+                */
+                particles.push(new Particle(x,y))
+            }
+            
+        }
     }
     console.log(particles)
 }
@@ -54,8 +89,9 @@ function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     particles.forEach(particle => {
         particle.draw();
+        particle.update();
     })
-    
+
     requestAnimationFrame(animate)
 }
 
